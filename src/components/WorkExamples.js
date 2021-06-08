aa00aba7fe1f8342f7f39d11a71007c7022aa770
Yuliia <julia.av.ferreira@gmail.com>
2021-06-08 15:47:20 +0200

    added more works

--- src/components/WorkExamples.js
+++ src/components/WorkExamples.js
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import holidaze from "../image/holidaze.png";
 import got from "../image/got.png";
 import coctails from "../image/coctails.png";
+import bootstrap from "../image/bootstrap.png";
+import flag from "../image/flag.png";
+import port from "../image/port.png";
 import { motion } from "framer-motion";
 import {
   pageAnimation,
@@ -16,48 +19,69 @@ import Scroll from "../components/Scroll";
 const WorkExamples = () => {
   const [el, control] = useScroll();
   const [el2, control2] = useScroll();
-  return ( <div className="container">
-    <Work
-      style={{ background: "#ffffff" }}
-      exit="exit"
-      variants={pageAnimation}
-      initial="hidden"
-      animate="show"
-    >
-     
-      <Mov>
-        <motion.h2 variants={fade}>Game of Thrones</motion.h2>
-        <motion.div variants={lineAnim} className="line"></motion.div>
-        <Link to="https://got.y-ferreira.eu/">
-          <Hide>
-            <motion.img
-              variants={photo}
-              src={got}
-              alt="got"
-              className="imgWork"
-            />
-          </Hide>
-        </Link>
-      </Mov>
+  return (
+    <div className="container">
+      <Work
+        style={{ background: "#ffffff" }}
+        exit="exit"
+        variants={pageAnimation}
+        initial="hidden"
+        animate="show"
+      >
+        <Mov>
+          <motion.h2 variants={fade}>Game of Thrones</motion.h2>
+          <motion.div variants={lineAnim} className="line"></motion.div>
+          <Link to="https://got.y-ferreira.eu/">
+            <Hide>
+              <motion.img
+                variants={photo}
+                src={got}
+                alt="got"
+                className="imgWork"
+              />
+            </Hide>
+          </Link>
+        </Mov>
 
-      <Mov ref={el} variants={fade} animate={control} initial="hidden">
-        <h2>Holidaze</h2>
-        <motion.div variants={lineAnim} className="line"></motion.div>
-        <Link to="https://holidaze1.vercel.app/">
-          <img src={holidaze} alt="holidaze" className="imgWork" />
-        </Link>
-      </Mov>
+        <Mov ref={el} variants={fade} animate={control} initial="hidden">
+          <h2>Holidaze</h2>
+          <motion.div variants={lineAnim} className="line"></motion.div>
+          <Link to="https://holidaze1.vercel.app/">
+            <img src={holidaze} alt="holidaze" className="imgWork" />
+          </Link>
+        </Mov>
+        <Mov>
+          <motion.h2 variants={fade}>Coctails API</motion.h2>
+          <motion.div variants={lineAnim} className="line"></motion.div>
+          <Link to="https://tasks.y-ferreira.eu/">
+            <img src={coctails} alt="coctails" className="imgWork" />
+          </Link>
+        </Mov>
+        <Mov>
+          <motion.h2 variants={fade}>Example of portfolio</motion.h2>
+          <motion.div variants={lineAnim} className="line"></motion.div>
 
-      <Mov ref={el2} variants={fade} animate={control2} initial="hidden">
-        <h2>Coctails API</h2>
-        <motion.div variants={lineAnim} className="line"></motion.div>
-        <Link to="https://tasks.y-ferreira.eu/">
-          <img src={coctails} alt="coctails" className="imgWork" />
-        </Link>
-      </Mov>
-      
-      <Scroll />
-    </Work>
+          <Link to="https://dark.y-ferreira.eu/">
+            <img src={bootstrap} alt="bootstrap" className="imgWork" />
+          </Link>
+        </Mov>
+        <Mov>
+          <motion.h2 variants={fade}>Countries API</motion.h2>
+          <motion.div variants={lineAnim} className="line"></motion.div>
+
+          <Link to="https://js-frameworks-module-assignment-4-yuliiafer.vercel.app/">
+            <img src={flag} alt="countries" className="imgWork" />
+          </Link>
+        </Mov>
+        <Mov ref={el2} variants={fade} animate={control2} initial="hidden">
+          <h2>Portfolio</h2>
+          <motion.div variants={lineAnim} className="line"></motion.div>
+          <Link to="https://portfolio-old.y-ferreira.eu/">
+            <img src={port} alt="portfolio" className="imgWork" />
+          </Link>
+        </Mov>
+        <Scroll />
+      </Work>
     </div>
   );
 };
